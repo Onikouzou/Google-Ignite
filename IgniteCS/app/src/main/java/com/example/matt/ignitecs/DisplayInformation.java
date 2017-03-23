fff1668557f0edd68039fa67771cc5ef6dde849d
Matt Neis <neismj12@uwgb.edu>
2017-03-22 22:31:55 -0500

    Dev Update 007
    JAMES BOND EDITION
    
    -Added camera functionality (incomplete so far)

--- IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
+++ IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
@@ -1,9 +1,13 @@
 package com.example.matt.ignitecs;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Location;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -27,7 +31,8 @@ public class DisplayInformation extends AppCompatActivity
         setContentView(R.layout.activity_display_information);
 
         // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
+        if (mGoogleApiClient == null)
+        {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addApi(LocationServices.API)
                     .addConnectionCallbacks(this)
@@ -36,6 +41,11 @@ public class DisplayInformation extends AppCompatActivity
         }
 
         txtLocCoarse = (TextView) findViewById(R.id.txtLocCoarse);
+
+        // Create camera
+        int CAMERA_PIC_REQUEST = 1337;
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
 
     } // end onCreate
 
@@ -63,6 +73,9 @@ public class DisplayInformation extends AppCompatActivity
         }
     }
 
+    /*
+     * The following are for google api
+     */
     @Override
     public void onConnectionSuspended(int cause) {
         //your code goes here
@@ -83,4 +96,23 @@ public class DisplayInformation extends AppCompatActivity
         mGoogleApiClient.disconnect();
         super.onStop();
     }
+
+    /*
+    The following methods are for the controlling the camera
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1337)
+        {
+            // data.getExtras()
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        }
+        else
+        {
+            Toast.makeText(DisplayInformation.this, "Pictures Not Taken", Toast.LENGTH_LONG);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 } // end class
