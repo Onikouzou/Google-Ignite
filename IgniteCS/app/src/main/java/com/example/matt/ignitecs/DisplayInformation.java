75d6f0c908cc0d0e2047e7e26cb9ac29a1c07316
Vince Cefalu <VinceCefalu@yahoo.com>
2017-03-25 20:00:44 -0500

    Dev Update 09

--- IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
+++ IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
@@ -16,9 +16,17 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
 public class DisplayInformation extends AppCompatActivity
         implements OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks, LocationListener
+        GoogleApiClient.ConnectionCallbacks, LocationListener,
+        OnMapReadyCallback
 {
 
     private GoogleApiClient mGoogleApiClient;
@@ -41,6 +49,12 @@ public class DisplayInformation extends AppCompatActivity
         }
 
         txtLocCoarse = (TextView) findViewById(R.id.txtLocCoarse);
+
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         // Create camera
         int CAMERA_PIC_REQUEST = 1337;
@@ -98,7 +112,7 @@ public class DisplayInformation extends AppCompatActivity
     }
 
     /*
-    The following methods are for the controlling the camera
+     * The following methods are for the controlling the camera
      */
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
@@ -112,6 +126,25 @@ public class DisplayInformation extends AppCompatActivity
             Toast.makeText(DisplayInformation.this, "Pictures Not Taken", Toast.LENGTH_LONG);
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 
