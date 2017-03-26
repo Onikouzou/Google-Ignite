c8a6b4a7962c48136227b7b1c6de3e76acf2f911
Matt Neis <neismj12@uwgb.edu>
2017-03-25 19:59:37 -0500

    Dev Update 008
    Added functionality with the camera based on a 1x1 button. Currently
    running into issues with saving at a specific file path, as it breaks
    the app. I currently have it commented out, but if you need to do
    anything with it just uncomment it

--- IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
+++ IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
@@ -3,9 +3,15 @@ package com.example.matt.ignitecs;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +22,9 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+import java.io.File;
+import java.io.IOException;
+
 public class DisplayInformation extends AppCompatActivity
         implements OnConnectionFailedListener,
         GoogleApiClient.ConnectionCallbacks, LocationListener
@@ -23,6 +32,11 @@ public class DisplayInformation extends AppCompatActivity
 
     private GoogleApiClient mGoogleApiClient;
     TextView txtLocCoarse;
+    Button btnTakePicture;
+    ImageView displayPicture;
+
+    int CAMERA_PIC_REQUEST = 1337;
+    public static int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -42,10 +56,52 @@ public class DisplayInformation extends AppCompatActivity
 
         txtLocCoarse = (TextView) findViewById(R.id.txtLocCoarse);
 
+        /*
+        the following has to do with the camera
+         */
+
+        // create directory named igniteCSPics to store pics taken by camera using this application
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                + "/igniteCSPics/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+
+        // create button
+        btnTakePicture = (Button) findViewById(R.id.btnTakePicture);
+        btnTakePicture.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                // counter will be incremented each time to save the pics taken by the camera as
+                // 1.jpg, 2.jpg, etc.
+                count++;
+                String file = dir + count + ".jpg";
+                File newFile = new File(file);
+
+                // create new file in directory igniteCSPics
+                try{
+                    newFile.createNewFile();
+                }
+                catch(IOException e)
+                {
+                }
+
+                Uri outputFileUri = Uri.fromFile(newFile);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+            }
+        });
+
         // Create camera
-        int CAMERA_PIC_REQUEST = 1337;
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+
+        // enable to automatically click button when displayinformation is called
+       // btnTakePicture.callOnClick();
+
 
     } // end onCreate
 
@@ -100,19 +156,27 @@ public class DisplayInformation extends AppCompatActivity
     /*
     The following methods are for the controlling the camera
      */
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (requestCode == 1337)
-        {
-            // data.getExtras()
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-        }
-        else
-        {
-            Toast.makeText(DisplayInformation.this, "Pictures Not Taken", Toast.LENGTH_LONG);
-        }
         super.onActivityResult(requestCode, resultCode, data);
-    }
 
+        if (requestCode == CAMERA_PIC_REQUEST && resultCode == RESULT_OK)
+        {
+            Log.d("DisplayInformation", "Pic Saved");
+        }
+//        if (requestCode == CAMERA_PIC_REQUEST)
+//        {
+//            // data.getExtras()
+//            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+//        }
+//        else
+//        {
+//            Toast.makeText(DisplayInformation.this, "Pictures Not Taken", Toast.LENGTH_LONG);
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
 
 } // end class
