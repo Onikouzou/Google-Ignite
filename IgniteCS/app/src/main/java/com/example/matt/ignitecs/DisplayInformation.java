011d4599fd7e7608675da39616199238af1b6080
Vince Cefalu <VinceCefalu@yahoo.com>
2017-03-29 21:31:06 -0500

    Dev Update [RECORDING YOUR SECRETS]
    - Added mic recording codez
    - 1337 skillz were displayed

--- IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
+++ IgniteCS/app/src/main/java/com/example/matt/ignitecs/DisplayInformation.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+// Maps (coarse location)
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -22,13 +23,25 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
-
+// Maps (fine location)
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
+
+// Audio
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.widget.Button;
+import android.Manifest;
+import android.support.annotation.NonNull;
+import android.content.pm.PackageManager;
+import android.content.Context;
+import android.support.v4.app.ActivityCompat;
+import android.os.CountDownTimer;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -45,6 +58,21 @@ public class DisplayInformation extends AppCompatActivity
     Button btnTakePicture;
     ImageView displayPicture;
     Location mCurrentLocation;
+
+    // Audio vars
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String mFileName = null;
+
+    private MediaRecorder mRecorder = null;
+
+    private PlayButton   mPlayButton = null;
+    private MediaPlayer mPlayer = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
 
     int CAMERA_PIC_REQUEST = 1337;
     public static int count = 0;
@@ -72,6 +100,31 @@ public class DisplayInformation extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
             .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        // Audio Recording
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        // Attempt to record x seconds of audio
+        try {
+            onRecord(true);
+            // CountDownTimer( time in milliseconds, countdown interval )
+            CountDownTimer countDowntimer = new CountDownTimer(5000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                }
+
+                public void onFinish() {
+                    onRecord(false);
+                    // Create a player to play the recently recorded audio.
+                }};countDowntimer.start();
+        } catch (SecurityException e) {
+            // ERROR
+        }
+
 
         /*
         the following has to do with the camera
@@ -142,7 +195,7 @@ public class DisplayInformation extends AppCompatActivity
             mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
             txtLocCoarse.setText(mCurrentLocation.getLatitude() + ", " + mCurrentLocation.getLongitude());
         } catch (SecurityException e) {
-
+            // ERROR
         }
     }
 
@@ -155,8 +208,7 @@ public class DisplayInformation extends AppCompatActivity
     }
 
     @Override
-    public void onLocationChanged(Location loc)
-    {
+    public void onLocationChanged(Location loc) {
 
     }
 
@@ -168,6 +220,16 @@ public class DisplayInformation extends AppCompatActivity
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
+
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 
     /**
@@ -220,6 +282,96 @@ public class DisplayInformation extends AppCompatActivity
     }
 
 
+    // -----------------------
+    // Audio recording
+    // -----------------------
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    class PlayButton extends Button {
+        boolean mStartPlaying = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public PlayButton(Context ctx) {
+            super(ctx);
+            setText("Start playing");
+            setOnClickListener(clicker);
+        }
+    }
 
 
 } // end class
